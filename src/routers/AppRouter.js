cd45ce64076d5bbf0fe526d57b3a4f26d7bb86fc
cristiancsandoval <clinares668@gmail.com>
2022-03-24 20:03:57 -0500

    petición a la api

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Films from '../components/Films'
 import Header from '../components/Header'
@@ -6,6 +6,35 @@ import Starships from '../components/Starships'
 import Vehicles from '../components/Vehicles'
 
 const AppRouter = () => {
+
+    const [data, setData] = useState([]);
+    const endpoint = 'https://swapi.dev/api/films/';
+    const [loading, setLoading] = useState(false);
+
+    const getData = async() =>{
+        
+        setLoading(true);
+        const api = await fetch(endpoint);
+        const results = (await api.json()).results;
+        setData(results);
+        setLoading(false);
+
+    }
+
+    useEffect(()=>{
+
+        getData()
+
+    }, [])
+
+    if(loading){
+        return(
+            <div>
+                <h3>Cargando información...</h3>
+            </div>
+        )
+    }
+
   return (
     <BrowserRouter>
         <Header/>
