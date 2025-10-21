fe1c7aad746ad2671b02cffb2753d4583a78d849
Cristian Camilo Sandoval Linares <96083224+cristiancsandoval@users.noreply.github.com>
2025-10-20 19:09:47 -0500

    Delete src/data directory

--- src/data/getData.js
+++
@@ -1,8 +0,0 @@
-export const getData = async(endpoint, setData, setLoading) =>{
-      
-    const api = await fetch(endpoint);
-    const results = (await api.json()).results;
-    setData(results);
-    setLoading(false);
-
-}
